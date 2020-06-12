fe3ac23d24d2ff6308b7c7abbd9c757a8629fc52
Auguinard Kouame <kouameaugui@gmail.com>
2020-06-12 07:28:27 +0000

    new commit

--- operateurs/liste-des-danger-ajouter.php
+++ operateurs/liste-des-danger-ajouter.php
@@ -93,7 +93,7 @@ require_once "../php/db.php";
 
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img class="img-profile rounded-circle" src="../img/user.png">&nbsp;&nbsp;
+                                    <img class="img-profile rounded-circle" src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png">&nbsp;&nbsp;
                                     <span class="d-none d-lg-inline text-gray-600 small" style="color: #fff !important;font-weight: 800;">
                                     <?= htmlspecialchars($_SESSION["prenom"]); ?>
                                 </span>
